9bc072005dae54e9770670496fa1c0b225bd7025
Charles Heidemann <charles.heidemann@gmail.com>
2017-08-24 18:46:49 -0300

    aa

--- enron_indexer/entrypoint.sh
+++ enron_indexer/entrypoint.sh
@@ -1,3 +1,3 @@
 # Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
 # See here https://docs.docker.com/engine/reference/builder/#/entrypoint
-exec java -jar $ARTIFACT_NAME
+exec java -version
