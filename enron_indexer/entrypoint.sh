4fb35b9818ccdf0fc8740212b3f01aea661029b4
Charles Heidemann <charles.heidemann@gmail.com>
2017-08-24 18:44:32 -0300

    Docckefile

--- enron_indexer/entrypoint.sh
+++ enron_indexer/entrypoint.sh
@@ -1,4 +1,3 @@
 # Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
 # See here https://docs.docker.com/engine/reference/builder/#/entrypoint
-exec java $JAVA_OPTS \
-       -Djava.security.egd=file:/dev/./urandom -jar $ARTIFACT_NAME
+exec java -jar $ARTIFACT_NAME
