9e24f2c75c8c1361bae2ad232a8c11aa302dc284
Charles Heidemann <charles@example.com>
2017-08-25 15:13:34 -0300

    script

--- enron_web/entrypoint.sh
+++ enron_web/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -version
+java -Dserver.port=$SERVER_PORT -Delasticsearch.host=$ES_HOST -Delasticsearch.port=$ES_PORT -Delasticsearch.clustername=$ES_CLUSTER -Djava.security.egd=file:/dev/./urandom -jar $ARTIFACT_NAME
